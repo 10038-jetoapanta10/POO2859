5b60686cfb468f81fec4b06fcb9d07f302df2392
ivanalejandrozambrano <65724423+ivanalejandrozambrano@users.noreply.github.com>
2021-02-17 13:55:12 -0500

    Update hiMundo.java
    agrague una variable

--- hiMundo.java
+++ hiMundo.java
@@ -1,9 +1,11 @@
 public class HolaMundo{
 
 public static void main (String[] args){
+  int n1=99;
 System.out.println("Hola mundo uwu");
 System.out.println("nuevo cambio");  
 System.out.println("cambio"); 
+  System.out.println(n1); 
 }
 
 }
