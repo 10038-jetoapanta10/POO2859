cc499759d3b410c299e131441085573abcca34f5
Pablo Parra <pxparra@espe.edu.ec>
2021-02-17 13:57:42 -0500

    Update hiMundo.java
    Hice cambios para mostrar 10 veces los mensajes

--- hiMundo.java
+++ hiMundo.java
@@ -2,10 +2,14 @@ public class HolaMundo{
 
 public static void main (String[] args){
   int n1=99;
+
+  System.out.println(n1); 
+for(int i=0;i<10;i++){
 System.out.println("Hola mundo uwu");
 System.out.println("nuevo cambio");  
 System.out.println("cambio"); 
-  System.out.println(n1); 
+}
+
 }
 
 }
