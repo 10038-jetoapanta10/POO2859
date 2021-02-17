412212c27551f6b7b653278a8e546cb3e21e2efd
Elian <trabajoct@outlook.com>
2021-02-17 13:43:50 -0500

    primer programa

---
+++ hiMundo.java
@@ -0,0 +1,10 @@
+public class HolaMundo{
+
+public static void main (String[] args){
+System.out.println("Hola mundo uwu");
+System.out.println("nuevo cambio");  
+System.out.println("cambio"); 
+}
+
+}
+
